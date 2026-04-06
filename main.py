aa93e0656591a410b3fef01d5e774d5a7f007419
cdkbusiness0710 <cdkbusiness0710@gmail.com>
2026-04-06 12:34:26 +0530

    Update and rename python1.py to main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+rom flask import flask,render_template
+app=flask(__name__)
+@app.route('/')
+def home():
+  @ render_templete automatically looks in the /templetes folder
+  return render_template('index.html',title='Home page')
+if __name__='__main__':
+app.run(host='127.0.0.1',port=8080 ,debug =True)
